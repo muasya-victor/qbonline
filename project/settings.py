63ff04d5d11f5767b33a56b15145c7f37c59407b
muasya-victor <vicmwe184@gmail.com>
2025-10-25 12:33:54 +0300

    update server timeout

--- project/settings.py
+++ project/settings.py
@@ -126,7 +126,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         'OPTIONS': {
-            'timeout': 60, 
+            'timeout': 420, 
         }
     }
 }
