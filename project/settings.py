0d769ce0ecb8bdc782147d0277d6952043dd12b0
muasya-victor <vicmwe184@gmail.com>
2025-11-20 12:20:24 +0300

    allow staging

--- project/settings.py
+++ project/settings.py
@@ -90,10 +90,10 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://localhost:3001",  # Frontend dev server
+    "http://localhost:3001",  
     "http://212.47.77.18",
     "https://qbo-ui.netlify.app",
-    "https://qbo-ui-staging.netlify.app",
+    "https://qb-ui-staging.netlify.app",
 ]
 
 
